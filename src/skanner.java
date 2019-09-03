d33445135401b240d7e531129a9a36c8515c5498
JonathanLilie <JonathanLilie@users.noreply.github.com>
2019-09-03 15:27:04 +0200

    commit5

--- src/skanner.java
+++ src/skanner.java
@@ -10,6 +10,13 @@ public class skanner {
 			Integer age = Integer.parseInt(input.nextLine());
 			System.out.println("adress?");
 			String adress = input.nextLine();
+			System.out.println("postnummer?");
+			String postnummer = input.nextLine();
+			System.out.println("Stad?");
+			String stad = input.nextLine();
+			System.out.println("Telefonnummer?");
+			String telefonnummer = input.nextLine();
+			
 			
 			
 	}
