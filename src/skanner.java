dc76597e85cebec8df9a474e4a0401c204659035
JonathanLilie <JonathanLilie@users.noreply.github.com>
2019-09-03 15:18:54 +0200

    commit2

--- src/skanner.java
+++ src/skanner.java
@@ -4,5 +4,7 @@ public class skanner {
 	
 	public static void main(String[] args) {
 			Scanner input = new Scanner(System.in);
+			System.out.println("username?");
+			String username = input.nextLine(); 
 	}
 }
